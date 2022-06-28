675e88cc5e34db4ac904e3bf041eb8a3cab8a11c
coolpiece <yyrhsc@naver.com>
2022-06-28 14:22:23 +0900

    4673

---
+++ 한서윤/4673.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int d(int m);
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	bool check[10005] = { false, };
+	for (int i = 1; i < 10001; i++) {
+		int num = d(i);
+		if (num < 10001)
+			check[num] = true;
+	}
+	for (int i = 1; i < 10001; i++) {
+		if (check[i] == false)
+			cout << i << "\n";
+	}
+}
+
+int d(int n) {
+	int result = n;
+	while (n) {
+		result += n % 10;
+		n /= 10;
+	}
+	return result;
+}
