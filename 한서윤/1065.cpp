ca85cfd7b4fdd8c451b2918e580649e1982926ae
coolpiece <yyrhsc@naver.com>
2022-06-28 15:08:40 +0900

    1065

---
+++ 한서윤/1065.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	int n, cnt = 0, t1, t2, t3;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (i / 100 == 0) cnt++;
+		else {
+			t1 = i / 100;
+			t2 = i / 10 % 10;
+			t3 = i % 10;
+			if (t1 - t2 == t2 - t3) cnt++;
+		}
+	}
+	cout << cnt;
+}
