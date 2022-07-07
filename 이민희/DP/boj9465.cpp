59f39c00865d30346d43935df52db0217fecee35
minhee020 <minhee020@naver.com>
2022-07-07 18:19:15 +0900

    silver 1 sticker

--- 이민희/DP/boj9465.cpp
+++ 이민희/DP/boj9465.cpp
@@ -3,31 +3,39 @@ using namespace std;
 
 int main()
 {
+	int result;
 	int T;
 	int n;
 
 	cin >> T;
-	cin >> n;
 
-	int dp[2][n+1];
-	int a[2][n+1];
-
-	// input
-	for (int i = 0; i < 2; i++)
-		for (int j = 1; j <= n; j++)
-			cin >> a[i][j];
-
-	dp[0][0] = dp[0][1] = 0;
-	dp[0][1] = a[0][1];
-	dp[1][1] = a[1][1];
-
-	for (int i = 2; i <= n; i++)
+	for (int k = 0; k < T; k++)
 	{
-		dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + a[0][i];
-		dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + a[1][i];
+		//input
+		cin >> n;
+
+		int dp[2][n+1];
+		int a[2][n+1];
+
+		for (int i = 0; i < 2; i++)
+			for (int j = 1; j <= n; j++)
+				cin >> a[i][j];
+
+		dp[0][0] = dp[1][0] = 0;
+		dp[0][1] = a[0][1];
+		dp[1][1] = a[1][1];
+
+		for (int i = 2; i <= n; i++)
+		{
+			dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + a[0][i];
+			dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + a[1][i];
+		}
+
+		result = max(dp[0][n], dp[1][n]);
+
+		cout << result << '\n';
 	}
 
-	return max(dp[0][n], dp[1][n]);
 }
 
 //참고 링크 : https://m.blog.naver.com/occidere/220786307316
