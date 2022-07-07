c1c1bacdbf0516f1f3dcb7e7d02041f2e42877fd
minhee020 <minhee020@naver.com>
2022-07-07 18:12:48 +0900

    DP

--- 이민희/DP/boj9465.cpp
+++ 이민희/DP/boj9465.cpp
@@ -1,44 +1,33 @@
 #include <iostream>
 using namespace std;
 
-enum Place
-{
-	u, d
-};
-
-int find_maxSum(int **d, int n)
-{
-
-	int **a = d;
-	for (int i = 2; i <= n; i++) {
-		d[0][i] = max(d[1][i - 1], d[1][i - 2]) + a[0][i + 1];
-		d[1][i] = max(d[0][i - 1], d[0][i - 2]) + a[1][i + 1];
-	}
-
-	return max(d[n][0], d[n][1]);
-}
-
 int main()
 {
 	int T;
 	int n;
 
 	cin >> T;
+	cin >> n;
 
-	for (int i = 0; i < T; i++)
+	int dp[2][n+1];
+	int a[2][n+1];
+
+	// input
+	for (int i = 0; i < 2; i++)
+		for (int j = 1; j <= n; j++)
+			cin >> a[i][j];
+
+	dp[0][0] = dp[0][1] = 0;
+	dp[0][1] = a[0][1];
+	dp[1][1] = a[1][1];
+
+	for (int i = 2; i <= n; i++)
 	{
-		cin >> n;
-		int nums[n + 1][2];
-
-		nums[0][0] = 0;
-		nums[0][1] = 0;
-
-		for (int j = 1; j <= n; j++)
-			cin >> nums[j][u];
-		for (int j = 1; j <= n; j++)
-			cin >> nums[j][d];
-		// cout << find_maxSum(nums, n);
+		dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + a[0][i];
+		dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + a[1][i];
 	}
+
+	return max(dp[0][n], dp[1][n]);
 }
 
 //참고 링크 : https://m.blog.naver.com/occidere/220786307316
