25fe543545b9c654fdf63e6ff62c4b83d989c225
minhee020 <minhee020@naver.com>
2022-07-10 14:09:42 +0900

    circular

--- 이민희/DFS & BFS/boj10451.cpp
+++ 이민희/DFS & BFS/boj10451.cpp
@@ -1,49 +1,44 @@
-// #include <iostream>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-// #define MAX 1001
+vector<int> a[1001];
+bool check[1001];
 
-// int T, N;
-// vector<int> graph[MAX];
-// int visited[MAX];
+void dfs(int node) {
+	check[node]=true;
+	for (int i=0; i<a[node].size(); i++) {
+		int next = a[node][i];
+		if (!check[next]) {
+			dfs(next);
+		}
+	}
+}
 
-// int DFS(int v)
-// {
-//     visited[v] = 1;
-//     static int next;
-//     int gsize = graph[v].size();
-//     for (int i = 0; i < gsize; i++){
-//         next = graph[v][i];
-//         if (!visited[next])
-//             DFS(next);
-//     }
-//     if (next == v)
-//         return 1;
-//     return 0;
-// }
-
-// int main(){
-
-//     int to, count;
-
-//     count = 0;
-//     cin >> T;
-//     while (T--)
-//     {
-//         cin >> N;
-//         for (int from = 1; from <= N; from++)
-//         {
-//             cin >> to;
-//             graph[from].push_back(to);
-//         }
-
-//         for (int i = 1; i <= N; i++)
-//         {
-//             if (!visited[i])
-//                 if (DFS(i))
-//                     count++;
-//         }
-//         cout << count << '\n';
-//     }
-// }
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i=1; i<=n; i++) {		// 사용한 공간 초기화
+			a[i].clear();
+			check[i]=false;
+		}
+		for (int i=1; i<=n; i++) {		// 방향 그래프 입력
+			int v;
+			cin >> v;
+			a[i].push_back(v);
+		}
+		int cnt=0;
+		for (int i=1; i<=n; i++) {		// 각 탐색마다 count + 1
+			if (!check[i]) {
+				dfs(i);
+				cnt++;
+			}
+		}
+		cout << cnt << '\n';
+	}
+	return 0;
+}
