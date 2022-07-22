c6eec005811bcf3378811b5bd638c4815a8aeb58
minhee020 <minhee020@naver.com>
2022-07-22 21:39:32 +0900

    card2

---
+++ 이민희/bi_ternary search/boj10816.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+
+bool find_pivot(int narr[], int pivot){
+    int left, right, mid;
+    left = 0;
+    right = n - 1;
+    
+    while (left <= right){
+        mid = (left + right) / 2;
+
+        if (narr[mid] == pivot)   return true;
+        if (narr[mid] > pivot) right = mid - 1;
+        if (narr[mid] < pivot) left = mid + 1;
+    }
+    return false;
+}
+
+int main(){
+    cin >> n;
+    int narr[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> narr[i];
+    sort(narr, narr + n);
+
+    cin >> m;
+    int marr[m];
+
+    for (int i = 0; i < m; i++)
+        cin >> marr[i];
+
+    for (int i = 0; i < m; i++){
+        if (find_pivot(narr, marr[i]))
+        {
+            cout << upper_bound(narr, narr + n, marr[i]) - lower_bound(narr, narr + n, marr[i]) << " ";
+        }
+        else
+            cout << "0 ";
+    }
+}
