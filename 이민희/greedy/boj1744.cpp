29c681ecee4fe04def1003972e65656dc12a1775
minhee020 <minhee020@naver.com>
2022-07-25 10:54:22 +0900

    1744.cpp

--- 이민희/greedy/boj1744.cpp
+++ 이민희/greedy/boj1744.cpp
@@ -11,24 +11,35 @@ int main(){
     cout.tie(0);
 
     cin >> n;
-    int i;
-    for (i = 0; i < n; i++){
-        cin >> arr[i];
+    int tmp;
+    for (tmp = 0; tmp < n; tmp++){
+        cin >> arr[tmp];
     }
 
-    sort(arr, arr + i, greater<int>());
+    sort(arr, arr + tmp, greater<int>());
 
-    int result = 0;
-    i--;
+    int greater = 0;
+    int i = tmp - 1;
     while(i >= 0){
         if (arr[i] > 0 && arr[i - 1] > 0)
-            result += arr[i] * arr[i - 1];
+            greater += arr[i] * arr[i - 1];
         else if (i - 1 < 0)
-            result += arr[i];
+            greater += arr[i];
         else
-            result += arr[i] + arr[i - 1];
+            greater += arr[i] + arr[i - 1];
         i -= 2;
     }
 
-    cout << result;
+    int less = 0;
+    while(tmp){
+        if (arr[i] > 0 && arr[i - 1] > 0)
+            greater += arr[i] * arr[i - 1];
+        else if (i - 1 < 0)
+            greater += arr[i];
+        else
+            greater += arr[i] + arr[i - 1];
+        i -= 2;
+    }
+
+    cout << greater;
 }
