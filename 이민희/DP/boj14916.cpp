913d65a16be59f62765f3e6c6857a4929139a2b1
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2022-11-23 12:57:19 +0900

    feat : S5 거스름돈 문제

---
+++ 이민희/DP/boj14916.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100000
+
+int main() {
+    int d[] = {5, 2};
+    int n;
+    cin >> n;
+    int c[n + 1];
+    for (int i = 1; i <= n; i++) {
+        c[i] = MAX;
+    }
+
+    c[0] = 0;
+
+    for (int j = 1; j <= n; j++){
+        for (int i = 0; i < 2; i++) {
+            if ((d[i] <= j) && (c[j - d[i]] + 1 < c[j])) {
+                c[j] = c[j - d[i]] + 1;
+            } 
+        }
+    }
+
+    cout << c[n];
+}
