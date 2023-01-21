7fed974c74ca5a77f59def5535b68eafb40d934a
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-01-21 14:31:11 +0900

    modi

--- 이민희/DP/boj11053.cpp
+++ 이민희/DP/boj11053.cpp
@@ -14,10 +14,10 @@ int main()
         dp[i] = 1;
     }
 
-    int sum = 0;
+    int sum = 1;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < i; j++) {
             if (arr[i] > arr[j]) {
                 dp[i] = max(dp[i], dp[j] + 1);
             }
