de2a4c834acecbe45cf95c1c5f2131c83e378fa3
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-02-03 18:43:59 +0900

    g4

--- 이민희/greedy/boj1744.cpp
+++ 이민희/greedy/boj1744.cpp
@@ -1,45 +1,56 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 
-int arr[51];
-int n;
+vector<int> positive;
+vector<int> negative;
+int result;
+int zero_count;
+
+int greedy() {
+    sort(positive.begin(), positive.end(), greater<int>());
+    sort(negative.begin(), negative.end(), less<int>());
+
+    for (int i = 0; i < positive.size() - (positive.size()%2); i += 2) {
+        result += positive[i] * positive[i + 1];
+    }
+    if (positive.size() % 2 == 1) {
+        result += positive.back();
+    }
+
+    for (int i = 0; i < negative.size() - (negative.size()%2); i += 2) {
+        result += negative[i] * negative[i + 1];
+    }
+    if ((negative.size() % 2 == 1) && (zero_count == 0)) {
+        result += negative.back();
+    }
+
+    return result;
+}
 
 int main(){
     ios_base::sync_with_stdio(0); 
     cin.tie(0); 
     cout.tie(0);
 
+    int n;
     cin >> n;
+
     int tmp;
-    for (tmp = 0; tmp < n; tmp++){
-        cin >> arr[tmp];
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        if (tmp > 1) {
+            positive.push_back(tmp);
+        } else if (tmp == 1) {
+            result += 1;
+        } else if (tmp == 0) {
+            zero_count += 1;
+        } else {
+            negative.push_back(tmp);
+        }
     }
 
-    sort(arr, arr + tmp, greater<int>());
-
-    int greater = 0;
-    int i = tmp - 1;
-    while(i >= 0){
-        if (arr[i] > 0 && arr[i - 1] > 0)
-            greater += arr[i] * arr[i - 1];
-        else if (i - 1 < 0)
-            greater += arr[i];
-        else
-            greater += arr[i] + arr[i - 1];
-        i -= 2;
-    }
-
-    int less = 0;
-    while(tmp){
-        if (arr[i] > 0 && arr[i - 1] > 0)
-            greater += arr[i] * arr[i - 1];
-        else if (i - 1 < 0)
-            greater += arr[i];
-        else
-            greater += arr[i] + arr[i - 1];
-        i -= 2;
-    }
-
-    cout << greater;
+    cout << greedy() << endl;
 }
