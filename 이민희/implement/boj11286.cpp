a818e07464a975c9892879bc86497ffaec12c54e
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-03-03 16:50:41 +0900

    놓친 조건

--- 이민희/implement/boj11286.cpp
+++ 이민희/implement/boj11286.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 struct comp {
     bool operator()(pair<int, int> A, pair<int, int> B) {
-        if (A.first > B.first)
+        if (A.first < B.first)
             return true;
         return false;
     }
@@ -21,14 +21,21 @@ int main() {
         cin >> tmp;
         if (tmp == 0) {
             if (pq.empty()) {
-                cout << 0 << '\n';
+                // cout << 0 << '\n';
                 result.push_back(0);
             } else {
-                cout << pq.top().second << '\n';
+                // cout << pq.top().second << '\n';
+                result.push_back(pq.top().second);
                 pq.pop();
             }
         } else {
             pq.push(make_pair(abs(tmp), tmp));
         }
     }
+    // cout << " ------------- " << endl;
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << endl;
+    }
+
+    // 놓친 조건 : 절댓값이 가장 작은 값이 여러개일 때는, 가장 작은 수를 출력하고, 그 값을 배열에서 제거한다.
 }
