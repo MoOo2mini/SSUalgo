161ae70e43b1706c57b248d62d98c81dc40df94e
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-03-05 17:25:55 +0900

    abs heap

--- 이민희/implement/boj11286.cpp
+++ 이민희/implement/boj11286.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 struct comp {
     bool operator()(pair<int, int> A, pair<int, int> B) {
-        if (A.first < B.first)
+        if (A.first > B.first)
             return true;
+        if (A.first == B.first) {
+            return A.second > B.second;
+        }
         return false;
     }
 };
@@ -32,10 +35,8 @@ int main() {
             pq.push(make_pair(abs(tmp), tmp));
         }
     }
-    // cout << " ------------- " << endl;
     for (int i = 0; i < result.size(); i++) {
         cout << result[i] << endl;
     }
 
-    // 놓친 조건 : 절댓값이 가장 작은 값이 여러개일 때는, 가장 작은 수를 출력하고, 그 값을 배열에서 제거한다.
 }
