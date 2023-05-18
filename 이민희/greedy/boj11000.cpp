f75e87486c000a80b113a8980367db8c50c7dcc3
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-05-18 15:45:44 +0900

    boj11000 : 맞왜틀?

--- 이민희/greedy/boj11000.cpp
+++ 이민희/greedy/boj11000.cpp
@@ -14,6 +14,7 @@ bool comp(pair<int, int> a, pair<int, int> b) {
 int main() {
     int n; cin >> n;
     int a, b;
+
     vector<pair<int, int> > tmp;
     for (int i = 0; i < n; i++) {
         cin >> a >> b;
@@ -21,20 +22,39 @@ int main() {
     }
 
     sort(tmp.begin(), tmp.end(), comp);
-    for (int i = 0; i < n; i++) {
-        cout << tmp[i].first << " " << tmp[i].second << endl;
-    }
-    // int room = 0;
-    // for (int i = 0; i < tmp.size(); i++) {
-    //     for (int idx = 0; idx <= room; idx++) {
-    //         if (cr[idx] <= tmp[i].first) {
-    //             cr[idx] = tmp[i].second;
-    //             cout << "class " << idx + 1 << endl;
-    //             break;
-    //         } else {
-    //             room++;
-    //         }
-    //     }
+    
+    // cout << "sorting" << endl;
+    // for (int i = 0; i < n; i++) {
+    //     cout << tmp[i].first << " " << tmp[i].second << endl;
     // }
-    // cout << room  + 1 << endl;
+
+    int room = 0;
+    int gap = 0;
+    int minGap = 200001;
+    int minIdx = -1;
+    for (int i = 0; i < tmp.size(); i++) {
+        minGap = 200001;
+        minIdx = -1;
+        gap = 0;
+        for (int idx = 0; idx <= room; idx++) {
+            gap = tmp[i].first - cr[idx];
+            if (gap < minGap && gap >= 0) {
+                // cout << "mingap : " << gap << endl;
+                minGap = gap;
+                minIdx = idx;
+            } else {
+                // cout << "invalid gap : " << gap << "i : " << i << endl;
+            }
+        }
+        if (minIdx == -1) {
+            // cout << "invalid in" << endl;
+            room++;
+            minIdx = room;
+        }
+
+        cr[minIdx] = tmp[i].second;
+        // cout << minIdx << ", " << tmp[i].second << endl;
+    }
+
+    cout << room + 1 << endl;
 }
