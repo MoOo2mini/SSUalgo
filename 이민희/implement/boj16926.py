11acfffb7bf8c03737a762d05862d9e172ebf4c8
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-08-04 17:23:29 +0900

    i dont know why..

--- 이민희/implement/boj16926.py
+++ 이민희/implement/boj16926.py
@@ -1,36 +1,49 @@
-from copy import copy
+import sys
+import math
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+input = sys.stdin.readline
 
 n, m, r = map(int, input().split())
 arr = list(list(map(int, input().split())) for _ in range(n))
-result = [[0 for _ in range(m)] for _ in range(n)]
-for i in range(n) :
-        for j in range(m) :
-            result[i][j] = arr[i][j]
+
 count = 0
 
-div = ((n - 1) * 2 + (m - 1) * 2)
-r %= div
+# print(lcm(10, 6))
+mod = ((n - 1) * 2 + (m - 1) * 2)
+div = mod
+for i in range(0, min(n, m), 2) :
+    div = (n - i - 1) * 2 + (m - i - 1) * 2
+    mod = lcm(mod, div)
 
-while (r != 0) :
-    while (min(n, m) / 2 != count) :
+r %= mod
+maxcount = min(n, m) // 2
+for _ in range(r) :
+    for count in range(maxcount) :
+    # while (min(n, m) // 2 != count) :
+        prev = arr[count][count]
+        tmp = arr[count][count]
         for i in range(count, n - (count + 1), 1) :
-            result[i + 1][count] = arr[i][count]
+            tmp = arr[i + 1][count]
+            arr[i + 1][count] = prev
+            prev = tmp
 
         for i in range(count, m - (count + 1), 1) :
-            result[n - (count + 1)][i + 1] = arr[n - (count + 1)][i]
+            tmp = arr[n - (count + 1)][i + 1]
+            arr[n - (count + 1)][i + 1] = prev
+            prev = tmp
 
         for i in range(n - (count + 1), count, -1) :
-            result[i - 1][m - (count + 1)] = arr[i][m - (count + 1)]
+            tmp = arr[i - 1][m - (count + 1)]
+            arr[i - 1][m - (count + 1)] = prev
+            prev = tmp
 
         for i in range(m - (count + 1), count, -1) :
-            result[count][i - 1] = arr[count][i]
+            tmp = arr[count][i - 1]
+            arr[count][i - 1] = prev
+            prev = tmp
         
-        count += 1
-    for i in range(n) :
-        for j in range(m) :
-            arr[i][j] = result[i][j]
-    r -= 1
-    count = 0
 
-for i in result :
+for i in arr :
     print(*i)
