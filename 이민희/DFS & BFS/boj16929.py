6cc090a60ad20e8b920defd1959983f934829d66
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-09-04 17:54:54 +0900

    two dots

--- 이민희/DFS & BFS/boj16929.py
+++ 이민희/DFS & BFS/boj16929.py
@@ -1,4 +1,43 @@
-# 네모 모양을 그리면서 탐색.
-# DFS 문제인 것 같음.
-# visited 한 곳을 다시 탐색하지 않음.
-# 제자리로 돌아온 것을 잘 판별할 것!
+import sys
+input = sys.stdin.readline
+
+n, m = list(map(int, input().split()))
+arr = []
+for _ in range(n) :
+    arr.append(list(input())[:m:])
+
+# from collections import deque
+
+visited = [[0] * m for _ in range(n)]
+
+r = [-1, 1, 0, 0]
+c = [0, 0, 1, -1]
+answer = False
+
+def dfs(row, col, cnt, start_row, start_col) :
+    global answer
+    if answer :
+        return
+    
+    for i in range(4) :
+        rr = row + r[i]
+        cc = col + c[i]
+        if 0 <= rr < n and 0 <= cc < m :
+            if arr[rr][cc] == arr[row][col] :
+                if rr == start_row and cc == start_col and cnt >= 4 :
+                    answer = True
+                    return
+                elif visited[rr][cc] == 0 :
+                    visited[row][col] = 1
+                    dfs(rr, cc, cnt + 1, start_row, start_col)
+                    visited[rr][cc] = 0
+
+for i in range(n) :
+    for j in range(m) :
+        visited[i][j] = 1
+        dfs(i, j, 1, i, j)
+        if answer :
+            print('Yes')
+            exit()
+if not answer :
+    print('No')
